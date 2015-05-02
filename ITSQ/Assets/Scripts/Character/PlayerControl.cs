9b859408098dd0f30d0a2c4e70b08bbacebbe4e3
NeilDG <neil.dg@pocketplaylab.com>
2015-05-02 23:06:44 +0800

    Replaced character movement machine with player controller.

--- ITSQ/Assets/Scripts/Character/PlayerControl.cs
+++ ITSQ/Assets/Scripts/Character/PlayerControl.cs
@@ -12,24 +12,39 @@ public class PlayerControl : MonoBehaviour {
 	[SerializeField] private float speed = 200.0f;
 	[SerializeField] private float jumpSpeed = 200.0f;
 	[SerializeField] private float gravity = 150.0f;
+	[SerializeField] private float rotationSpeed = 10.0f;
 	
 	private Vector3 moveDirection = Vector3.zero;
 	private CharacterController controller;
+
+	private bool rotateable = false;
 
 	void Start() {
 		this.controller = this.GetComponent<CharacterController> ();
 	}
 	void Update() {
 		if (controller.isGrounded) {
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			moveDirection = transform.TransformDirection (moveDirection);
 			moveDirection *= speed;
-			if (Input.GetButton("Jump"))
+			if (Input.GetButton ("Jump"))
 				moveDirection.y = jumpSpeed;
 			
 		}
 
+		if (Input.GetMouseButtonDown (1)) {
+			this.rotateable = true;
+		} else if (Input.GetMouseButtonUp (1)) {
+			this.rotateable = false;
+		}
+
+		if (this.rotateable) {
+			float rotation = Input.GetAxis("Mouse X") * this.rotationSpeed;;
+			transform.Rotate(0,rotation, 0);
+		}
+
 		moveDirection.y -= gravity * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
+
 	}
 }
