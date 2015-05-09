7bdcd09d57f5678074af033689328410450056fc
NeilDG <neil.dg@pocketplaylab.com>
2015-05-09 15:17:18 +0800

    Setup for nav mesh.

---
+++ ITSQ/Assets/Scripts/Character/EnemyAI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	[SerializeField] private NavMeshAgent navMeshAgent;
+	[SerializeField] private Transform target;
+
+	private Transform playerLocation;
+
+	// Use this for initialization
+	void Start () {
+		this.playerLocation = GameObject.FindObjectOfType<PlayerControl> ().transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.navMeshAgent.SetDestination (this.target.position);
+	}
+}
